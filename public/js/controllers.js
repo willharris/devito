0a9f9eb19c0a59b2a4ffaecd4eff4ab24b657372
Will Harris <will@harris.ch>
2016-02-18 14:44:33 +0100

    Confirm deletion of URLs and focus the first input field after actions.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,11 +3,15 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-    .controller('UrlListController', ['$scope', 'Url',
-        function ($scope, Url) {
+    .controller('UrlListController', ['$document', '$scope', '$window', 'Url',
+        function ($document, $scope, $window, Url) {
             var urlList = this;
 
             $scope.newUrl = new Url();
+
+            $scope.focusInput = function () {
+                $document.find('input')[0].focus();
+            };
 
             $scope.updateUrls = function () {
                 Url.query(function (data) {
@@ -20,18 +24,27 @@ angular.module('myApp.controllers', [])
                     });
                     $scope.newUrl.idx = maxIdx + 1;
                 });
+                $scope.focusInput();
             };
 
             urlList.addUrl = function () {
                 $scope.newUrl.$save(function () {
                     $scope.updateUrls();
                 });
+                $scope.focusInput();
             };
 
             urlList.deleteUrl = function (shortLink) {
-                Url.delete({urlId: shortLink}, function () {
-                    $scope.updateUrls();
-                });
+                var hostUrl = document.getElementById('host_url').innerHTML;
+                if ($window.confirm('Users will no longer be able to resolve the URL\n\n' +
+                        hostUrl + '/' + shortLink +
+                        '\n\nas soon as you click OK!\n\n' +
+                        'Do you really want to delete this URL?')) {
+                    Url.delete({urlId: shortLink}, function () {
+                        $scope.updateUrls();
+                    });
+                }
+                $scope.focusInput();
             };
 
             $scope.updateUrls();
