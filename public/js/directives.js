7da24fe0ab1d07378f6c9ab8f7f0bc852f17f5c0
Will Harris <will@harris.ch>
2016-02-19 10:46:08 +0100

    Implement some basic validation for the URLs.

--- public/js/directives.js
+++ public/js/directives.js
@@ -7,4 +7,17 @@ angular.module('myApp.directives', [])
         return function(scope, elm, attrs) {
             elm.text(version);
         };
+    })
+    .directive('validShort', function () {
+        // TODO could probably solve this more easily with an ng-pattern or somesuch...
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ctrl) {
+                ctrl.$validators.validShort = function (modelValue, viewValue) {
+                    if (/^\//.test(viewValue)) {
+                        return false;
+                    }
+                };
+            }
+        }
     });
